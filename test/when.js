ef732d48fc5fa6df39cf2cde6598de52fbd409f3
shadow <jing_hua_000@qq.com>
2019-10-16 00:28:55 +0800

    test

--- test/when.js
+++ test/when.js
@@ -5,10 +5,10 @@ describe('test when', function () {
   
   it('test', function () {
 
-    // let cannotOver3 = f.when(f.gt(3), f.always('so big'))
+    let cannotOver3 = f.when(f.gt(3), f.always('so big'))
 
-    // eq(cannotOver3(5), 'so big')
-    // eq(cannotOver3(1), 1)
+    eq(cannotOver3(5), 'so big')
+    eq(cannotOver3(1), 1)
 
   })
 
