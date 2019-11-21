55a0f3a3f6c430a6ff10db57eacd580a17e55899
jing_hua <36701496+jinghua000@users.noreply.github.com>
2019-11-21 12:48:20 +0800

    Improve tests (#12)
    Improve tests

--- test/when.js
+++ test/when.js
@@ -2,13 +2,30 @@ const eq = require('./util/eq')
 const f = require('../src')
 
 describe('test when', function () {
-  
-  it('test', function () {
+
+  it('when first function succeed, should call the second function', function () {
 
     let cannotOver3 = f.when(f.gt(3), f.always('so big'))
 
     eq(cannotOver3(5), 'so big')
-    eq(cannotOver3(1), 1)
+
+  })
+
+  it('when first function failed, should return the supplied argument self', function () {
+
+    let obj = {}
+    let cond = f.when(f.propEq('aa', 'aa'), f.always('good'))
+
+    eq(cond(obj), obj)
+
+  })
+
+  it('when first function succeed, should pass the last argument to second function', function () {
+
+    let obj = {}
+    let cond = f.when(f.T, f.identity)
+
+    eq(cond(obj), obj)
 
   })
 
