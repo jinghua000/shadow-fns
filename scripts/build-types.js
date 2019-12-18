87f0fc07a1d102a349d8d00be25cf38f4fbc9c42
ken <ken.huang@rccchina.com>
2019-12-18 18:10:07 +0800

    update

---
+++ scripts/build-types.js
@@ -0,0 +1,25 @@
+const fs = require('fs-extra')
+const join = require('current-path-join')
+const chalk = require('chalk')
+// ...
+const f = require('../lib')
+const filePath = join('../types/index.d.ts')
+
+;(async () => {
+
+  await fs.ensureFile(filePath)
+
+  const outputArray = []
+  outputArray.push(`export declare const VERSION: string;`)
+
+  Object.keys(f).sort(f.asc).forEach(k => {
+    if (k === 'VERSION') return 
+    outputArray.push(`export declare const ${k}: (...args: any[]) => any;`)
+  })
+
+  await fs.writeFile(filePath, outputArray.join(`\n`), 'utf8')
+
+})()
+
+console.log(chalk.cyan('Build declare file successfully!'))
+console.log(chalk.green(filePath))
